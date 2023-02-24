b94ac338e8d27b3836402cec4a31cc337f8c188c
Hyeonvv <pphhww5017@gmail.com>
2023-02-24 23:49:13 +0900

    2/24

---
+++ ParkHyeonwoo/frontend/src/views/lecture/axiosTest/AxiosTestPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+   <h2>Vue에서 Spring으로 데이터 전송하기</h2>
+   <axios-test-form @submit="onSubmit"/> <!-- emit 통해서 submit 이벤트 발생 시 onSubmit 함수 실행 -->
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+import AxiosTestForm from '@/components/lecture/axiosTest/AxiosTestForm.vue'
+
+export default {
+   name: "AxiosTestPage",
+   components: { AxiosTestForm },
+   methods: {
+      onSubmit(payload) {
+         const { memberName, major } = payload
+         axios.post('http://localhost:7777/vue/first/receive-test', // post 방식으로 url로 데이터를 날림, axios 는 ajax 같은 역할(비동기방식)
+            { memberName, major } // memberName, major 전송
+         )
+         .then(() => {
+            alert('데이터 전송 완료!')
+         })
+         .catch(() => {
+            alert('문제 발생!')
+         })
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ParkHyeonwoo/frontend/src/views/lecture/emitTest/EmitTestBoardGame.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+   <div>{{ currentTurnShape }} 님의 턴입니다.</div>
+   <table-component border="1" style="text-align: center;"
+      :table-data="tableData"
+      :current-turn-shape="currentTurnShape"
+      @updateTurnShape="updateTurnShape"
+      @updateWinner="updateWinner"
+      @updateTableData="updateTableData"/>
+      <div v-if="winner">{{ winner }} 님의 승리</div>
+  </v-container>
+</template>
+
+<script>
+
+import TableComponent from "@/components/lecture/emitTest/TableComponent.vue"
+
+// 오목 게임
+// 바둑판, 승자 판정, 그 다음 아직 모르겠음.
+// 누구 차례인지 어떻게 알지 ? 
+// 'O', 'X' 표시로 순서를 구분하자!
+
+export default {
+   name: "EmitTestBoardGame",
+   components: {
+      TableComponent // local 커스텀 태그 지정 (table-component)
+   },
+   data() { // 자체적으로 가지고 있는 객체 혹은 함수의 형태로 vue 인스턴스가 사용할 정보 (지역변수)
+      return {
+         currentTurnShape: 'O',
+         // tableData[5][5]
+         tableData: [
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+         ],
+         winner: '',
+      }
+   },
+   methods: { // vue 인스턴스가 사용할 함수
+      updateTurnShape(receivedTurnShape) {
+         console.log('Board Game received Component info: ' + receivedTurnShape)
+         this.currentTurnShape = receivedTurnShape // methods 에서 data() 에 있는 변수에 접근하고 싶을 때 this 사용
+      },
+      updateWinner(receivedWinner) {
+         console.log("Main Board: winner received")
+         this.winner = receivedWinner
+      },
+      updateTableData() {
+         this.tableData = [
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+            ['', '', '', '', ''],
+         ]
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
