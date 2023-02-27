8d4aef0f19f51b303c0e59ee7f87e6a980aac7f9
jmj30040 <jmj30040@gmail.com>
2023-02-27 17:41:13 +0900

    init: [정명진] PR TEST

---
+++ JeongMyeongjin/src/components/lecture/componentTest/GlobalComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="addCounter">{{ counterValue }}</button>
+</template>
+
+<script>
+
+export default {
+    name: "GlobalComponent",
+    props: ['initialTest', 'option'],
+    data() {
+        return {
+            counterValue: this.initialTest,
+            incValue: this.option,
+        }
+    },
+    methods: {
+        addCounter() {
+            this.counterValue += this.incValue
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
