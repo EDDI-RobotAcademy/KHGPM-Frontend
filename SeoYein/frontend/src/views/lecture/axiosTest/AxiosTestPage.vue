f5311de850ab5a17ce7bbef4b7a0c7cd7e81f2f0
Fromredtoviolet <aprilartists@gmail.com>
2023-02-27 18:20:02 +0900

    init: [서예인] 저장소 초기화
    깃허브데스크탑 초기 세팅중

---
+++ SeoYein/frontend/src/views/lecture/axiosTest/AxiosTestPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <h2>Vue에서 Spring으로 데이터 전송하기</h2>
+    <axios-test-page-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+
+import axios from 'axios'
+import axiosTestPageForm from '@/components/lecture/axiosTest/AxiosTestForm.vue'
+
+export default {
+    name: "AxiosTestPage",
+    components: { axiosTestPageForm },
+    methods: {
+        onSubmit (payload) {
+            const { memberName, major } = payload
+            axios.post('http://localhost:7777/vue/first/receive-test',
+                { memberName, major }
+            )
+            .then(() => {
+                alert('데이터 전송 완료!')
+            })
+            .catch(() => {
+                alert('문제 발생!')
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
