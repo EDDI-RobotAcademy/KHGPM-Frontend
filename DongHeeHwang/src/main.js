364dc7edd4a01691d85810b5a1b687a5efc60e66
zinif0607 <125858642+zinif0607@users.noreply.github.com>
2023-02-27 18:15:31 +0900

    init[황동희]PR Test

---
+++ DongHeeHwang/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+
+import GlobalComponent from "@/components/lecture/componentTest/GlobalComponent.vue"
+
+Vue.config.productionTip = false
+
+// global-component 커스텀 태그 등록
+Vue.component(GlobalComponent.name, GlobalComponent)
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
