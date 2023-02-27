18938ae1ac9e08e9e5d41a08df4f24a3b526c148
ParkBmo <gh repo clone kh-academy-java/java-class-a8>
2023-02-27 16:03:41 +0900

    test

---
+++ ParkHyeonMin/homework1/src/views/lecture/axiosTest/AxiosWinnerTestPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+      <h2>승자찾기</h2>
+      <axios-test-form @submit="onSubmit"/>
+    </v-container>
+  </template>
+  
+  <script>
+  
+  import axios from 'axios'
+  import EmitTestBoardGame from '@/views/lecture/emitTest/EmitTestBoardGame.vue'
+  
+  export default {
+      name: "AxiosWinnerTestPage",
+      components: {EmitTestBoardGame},
+      methods: {
+        winner (payload){
+            const {winner} =payload
+            axios.post('http://localhost:7777/vue/first/winner-test',
+             {winner}
+            )
+        }
+      }
+  }
+  
+  </script>
+  
+  <style>
+  
+  </style>
