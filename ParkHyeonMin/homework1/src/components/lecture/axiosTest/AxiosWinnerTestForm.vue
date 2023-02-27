a784ff8b98ddf9e33ea43cce5778cff0f66b0739
ParkBmo <backend.bmo@gmail.com>
2023-02-27 19:27:54 +0900

    통커밋 수정 veiw,table,tr,td,axios

---
+++ ParkHyeonMin/homework1/src/components/lecture/axiosTest/AxiosWinnerTestForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "AxiosWinnerTestForm",
+    data() {
+        return {
+            winner: '승자',
+        }
+    },
+    methods: {
+      onSubmit() {
+          const {winner} = this
+          this.$emit('submit', {winner})
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
