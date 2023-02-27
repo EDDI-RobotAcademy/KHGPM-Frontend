93a6c69b587c15eb84b5748be9b685ab2c2b10fd
Sungyeon <ksy48562@gmail.com>
2023-02-27 19:44:12 +0900

    feat:[김성연] 오목 게임 Frontend 과제

---
+++ Kimsungyeon/1주차과제/src/components/lecture/axiosTest/AxiosTestForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <table>
+      <tr>
+        <td>이름</td>
+        <td>
+          <input type="text" placeholder="이름을 입력하세요" v-model="memberName"/>
+        </td>
+      </tr>
+
+      <tr>
+        <td>전공</td>
+        <td>
+          <input type="text" placeholder="전공을 입력하세요" v-model="major"/>
+        </td>
+      </tr>
+    </table>
+
+    <div>
+      <button type="submit">전송</button>
+    </div>
+  </form>
+</template>
+
+<script>
+
+export default {
+    name: "AxiosTestForm",
+    data () {
+        return {
+            memberName: '',
+            major: '',
+        }
+    },
+    methods: {
+        onSubmit () {
+            const { memberName, major } = this
+            this.$emit('submit', { memberName, major })
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
