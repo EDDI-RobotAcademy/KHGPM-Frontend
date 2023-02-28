69b4a2162d3f2fdd90e8da6084fa142ed80fd190
hjlee5701 <hjlee5701@gmail.com>
2023-02-28 21:28:03 +0900

    feat:[이현진] start~end 지정 안한 경우

--- LeeHyeonjin/diceGame/src/CustomRandom.java
+++ LeeHyeonjin/diceGame/src/CustomRandom.java
@@ -9,4 +9,21 @@ public class CustomRandom {
     public static int createCustomRandom (int start, int end) {
         return  random.nextInt(end-start+1)+start;
     }
+
+    public static int createRandom () {
+        return  random.nextInt(6)+1;
+    }
+
+    public static int createStartRandom (int start) {
+        return  random.nextInt((start+6)-start+1)+start;
+    }
+
+
+    public static int createEndRandom (int end) {
+        int start = end < 6 ? 1 : end-5;
+        return  random.nextInt(end-start+1)+start;
+    }
+
+
+
 }
