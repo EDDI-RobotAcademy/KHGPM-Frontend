ebae647dc31ee9b7389fd969184677b6e1657829
yongjin217 <abcd950217@gamil.com>
2023-03-03 17:59:18 +0900

    init: [윤용진]저장소 초기화

---
+++ YoonYongjin/frontend/src/views/lecture/board/jpaBoardListPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <h2>Vue + Spring + JPA Boards</h2>
+    <router-link :to="{name: 'JpaBoardRegisterPage'}">
+        게시물 작성
+    </router-link>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+    name: "JpaBoardRegisterPage"
+}
+</script>
+
+<style>
+
+</style>
