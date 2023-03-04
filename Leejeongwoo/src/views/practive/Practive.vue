8738da96e68de80686e7d5b87a71e7e551aeb7a6
jeongwoogithub <leejeongpop@gmail.com>
2023-03-04 12:19:54 +0900

    add:[이정우] 혼자 연습중인 폴더 추가

---
+++ Leejeongwoo/src/views/practive/Practive.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <h2>정우 연습 공간</h2>
+
+    <router-link :to="{name: 'VforPractive'}">
+      v-for연습
+    </router-link> 
+
+    <router-link :to="{name: 'OneRoom'}">
+      원룸 만들기
+    </router-link>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Practive",
+
+}
+</script>
+
+<style>
+
+</style>
