5037db0ae08e0f6bf0bed92450bbf1689cf1e2d6
yongjin217 <abcd950217@gamil.com>
2023-03-05 13:29:53 +0900

    feat:[윤용진] 게시물 등록 frontend 완료 [BOARDx]

--- YoonYongjin/src/views/lecture/board/jpaBoardRegisterPage.vue
+++ YoonYongjin/src/views/lecture/board/jpaBoardRegisterPage.vue
@@ -1,17 +1,32 @@
 <template>
-    <v-container>
-      <h2>게시글 작성</h2>
-      <jap-board-register-form @submit="noSubmit"/>
-    </v-container>
-  </template>
-  
-  <script>
-  
-  export default {
-      name: "JpaBoardRegisterPage", 
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+  <v-container>
+    <h2>게시물 작성</h2>
+    <jpa-board-register-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+import JpaBoardRegisterForm from '@/components/lecture/board/JpaBoardRegisterForm.vue'
+import { mapActions } from 'vuex'
+
+export default {
+    components: { JpaBoardRegisterForm },
+    name: "JpaBoardRegisterPage",
+    methods: {
+        ...mapActions ([
+            'requestCreateBoardToSpring'
+        ]),
+        async onSubmit (payload) {
+            await this.requestCreateBoardToSpring(payload)
+            await this.$router.push({
+                name: 'JpaBoardListPage'
+            })
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
