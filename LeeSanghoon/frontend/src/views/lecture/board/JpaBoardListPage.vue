5e4de06ec2c675167c2375d5ca0090f3f2700b9f
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-06 16:01:10 +0900

    feat: 게시물 목록 보여주기 UI 완성 [BOARDx]

--- LeeSanghoon/frontend/src/views/lecture/board/JpaBoardListPage.vue
+++ LeeSanghoon/frontend/src/views/lecture/board/JpaBoardListPage.vue
@@ -4,13 +4,30 @@
     <router-link :to="{ name: 'JpaBoardRegisterPage' }">
       게시물 작성
     </router-link>
+    <jpa-board-list :boards="boards"/>
   </v-container>
 </template>
 
 <script>
+import JpaBoardList from '@/components/lecture/board/JpaBoardList.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
+  components: { JpaBoardList },
   name: "JpaBoardListPage",
+  computed: {
+    ...mapState([
+      'boards'
+    ]),
+  },
+  mounted () {
+    this.requestBoardListToSpring()
+  },
+  methods: {
+    ...mapActions([
+      'requestBoardListToSpring'
+    ])
+  }
 }
 
 </script>
