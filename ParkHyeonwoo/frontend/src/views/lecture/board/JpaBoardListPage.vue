7fecaf4cfff16398518d829b2d2587cbc3512f68
Hyeonvv <pphhww5017@gmail.com>
2023-03-06 16:25:14 +0900

    feat: 게시판 리스트 UI

--- ParkHyeonwoo/frontend/src/views/lecture/board/JpaBoardListPage.vue
+++ ParkHyeonwoo/frontend/src/views/lecture/board/JpaBoardListPage.vue
@@ -4,10 +4,13 @@
     <router-link :to="{ name: 'JpaBoardRegisterPage' }"> <!-- link는 router의 이동하고자 하는 대상 name과 매핑 -->
       게시물 작성
     </router-link>
+    <jpa-board-list :boards="boards" />
   </v-container>
 </template>
 
 <script>
+import JpaBoardList from '@/components/lecture/board/JpaBoardList.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
    components: { JpaBoardList },
@@ -15,6 +18,14 @@ export default {
    computed: {
       ...mapState([
          'boards'
+      ]),
+   },
+   mounted() {
+      this.requestBoardListToSpring()
+   },
+   methods: {
+      ...mapActions([
+         'requestBoardListToSpring'
       ])
    }
 }
