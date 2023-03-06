c644da03091ff2338614bb2ee009626e8b6a7bb3
jonginhan <instantgogo@gmail.com>
2023-03-06 21:16:30 +0900

    feat: board CRUD 구현

--- JongInHan/Frontend/src/router/index.js
+++ JongInHan/Frontend/src/router/index.js
@@ -1,52 +1,90 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import BindTest from '@/views/lecture/bind/BindTest.vue';
-import ClickEventTest from '@/views/lecture/clickEvent/ClickEventTest.vue';
-import ComponentTest from '@/views/lecture/componentTest/ComponentTest.vue';
-import EmitTestBoardGame from '@/views/lecture/emitTest/EmitTestBoardGame.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-import AxiosTestPage from '@/views/lecture/axiosTest/AxiosTestPage1.vue'
+import BindTest from "@/views/lecture/bind/BindTest.vue"
+import ClickEventTest from "@/views/lecture/clickEvent/ClickEventTest.vue"
+import ComponentTest from "@/views/lecture/componentTest/ComponentTest.vue"
 
-Vue.use(VueRouter);
+import EmitTestBoardGame from "@/views/lecture/emitTest/EmitTestBoardGame.vue"
+
+import AxiosTestPage from "@/views/lecture/axiosTest/AxiosTestPage.vue"
+import JpaBoardListPage from "@/views/lecture/board/JpaBoardListPage.vue"
+import JpaBoardRegisterPage from "@/views/lecture/board/JpaBoardRegisterPage.vue"
+import JpaBoardReadPage from "@/views/lecture/board/JpaBoardReadPage.vue"
+import JpaBoardModifyPage from "@/views/lecture/board/JpaBoardModifyPage.vue"
+
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: HomeView
   },
   {
     path: '/bind-test',
     name: 'BindTest',
-    component: BindTest,
+    component: BindTest
   },
   {
-    path: '/click-event-test',
+    path: '/click-test',
     name: 'ClickEventTest',
-    component: ClickEventTest,
+    component: ClickEventTest
   },
   {
     path: '/component-test',
     name: 'ComponentTest',
-    component: ComponentTest,
+    component: ComponentTest
   },
   {
-    path: '/emit-test',
+    path: '/emit-test-board-game',
     name: 'EmitTestBoardGame',
-    component: EmitTestBoardGame,
+    component: EmitTestBoardGame
   },
   {
-    path: '/axios-test',
+    path: '/axios-test-page',
     name: 'AxiosTestPage',
-    component: AxiosTestPage,
+    component: AxiosTestPage
   },
-];
+  {
+    path: '/board-list-page',
+    name: 'JpaBoardListPage',
+    component: JpaBoardListPage
+  },
+  {
+    path: '/board-register',
+    name: 'JpaBoardRegisterPage',
+    component: JpaBoardRegisterPage
+  },
+  {
+    path: '/board-read/:boardId',
+    name: 'JpaBoardReadPage',
+    components: {
+      default: JpaBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/board-modify/:boardId',
+    name: 'JpaBoardModifyPage',
+    components: {
+      default: JpaBoardModifyPage
+    },
+    props: {
+      default: true
+    }
+  },
+
+  
+]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
