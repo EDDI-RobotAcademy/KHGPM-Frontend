d1fe813279c11f7b2551f62a59bcd307a4a338b7
dduru-nuna <syj00062@gmail.com>
2023-03-06 16:38:45 +0900

    fix: [서윤지] states -> state

--- SeoYoonji/src/store/index.js
+++ SeoYoonji/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import states from '@/store/states'
+import state from '@/store/states'
 import actions from '@/store/actions'
 import mutations from '@/store/mutations'
 import getters from '@/store/getters'
@@ -9,7 +9,9 @@ import getters from '@/store/getters'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  states,
+  // states 가 아닌 state 인 이유
+  // states 가 예약어이기 때문에 js코드가 이상한 동작을 하게 됨
+  state,
   actions,
   mutations,
   getters,
