b91a573c144febf12622eae47c537483c339d3da
Hyeonvv <pphhww5017@gmail.com>
2023-03-06 16:25:44 +0900

    feat: 리스트 Backend 에 요청하는 API

--- ParkHyeonwoo/frontend/src/store/actions.js
+++ ParkHyeonwoo/frontend/src/store/actions.js
@@ -1,20 +1,26 @@
 import {
-    REQUEST_CREATE_BOARD_DATA_TO_SPRING
+   REQUEST_BOARD_LIST_TO_SPRING
 } from './mutation-types'
 
 import axios from 'axios'
 
 export default {
-    requestCreateBoardToSpring ({}, payload) {
+   requestCreateBoardToSpring ({}, payload) {
 
-        const { title, content, writer } = payload
-        return axios.post('http://localhost:7777/board/register',
-            { title, content, writer })
-            .then(() => {
-                alert('게시물 등록 성공!')
-            })
-            .catch(() => {
-                alert('문제 발생!')
-            })
-    }
+       const { title, content, writer } = payload
+       return axios.post('http://localhost:7777/board/register',
+           { title, content, writer })
+           .then(() => {
+               alert('게시물 등록 성공!')
+           })
+           .catch(() => {
+               alert('문제 발생!')
+           })
+   },
+   requestBoardListToSpring ({ commit }) {
+       return axios.get('http://localhost:7777/board/list')
+           .then((res) => {
+               commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
+           })
+   }
 }
---
+++ ParkHyeonwoo/frontend/src/store/mutations.js
@@ -0,0 +1,9 @@
+import {
+   REQUEST_BOARD_LIST_TO_SPRING,
+} from './mutation-types'
+
+export default {
+   [REQUEST_BOARD_LIST_TO_SPRING] (state, boards) {
+       state.boards = boards
+   }
+}
