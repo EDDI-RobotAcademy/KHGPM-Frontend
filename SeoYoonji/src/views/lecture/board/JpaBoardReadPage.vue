b3ac1bda7082f766f2f8f2517417c869b4d3277d
dduru-nuna <syj00062@gmail.com>
2023-03-06 20:44:03 +0900

    feat: [서윤지] 게시물 삭제 기능 추가

--- SeoYoonji/src/views/lecture/board/JpaBoardReadPage.vue
+++ SeoYoonji/src/views/lecture/board/JpaBoardReadPage.vue
@@ -4,6 +4,9 @@
         <h2>Vue + Spring 게시판 읽기</h2>
         <jpa-board-read v-if="board" :board="board"/>
         <p v-else>로딩중 ...........</p>
+        <router-link :to="{ name: 'JpaBoardModifyPage', params: { boardId } }">
+            게시물 수정
+        </router-link>
         <button @click="onDelete">삭제</button>
         <router-link :to="{ name: 'JpaBoardListPage' }">
             돌아가기
@@ -31,9 +34,11 @@ export default {
     methods: {
         ...mapActions([
             'requestBoardToSpring',
+            'requestDeleteBoardToSpring'
         ]),
-        onDelete() {
-
+        async onDelete() { //교실 네트워크 문제로 async await 걸어둠
+            await this.requestDeleteBoardToSpring(this.boardId)
+            await this.$router.push({ name: 'JpaBoardListPage' })
         },
     },
     created () {
