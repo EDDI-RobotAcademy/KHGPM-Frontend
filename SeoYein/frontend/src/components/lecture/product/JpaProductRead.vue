f9e7af67e04b943bd2b9b8ab6e6b7612c9513cf3
Fromredtoviolet <aprilartists@gmail.com>
2023-03-07 23:38:26 +0900

    feat: [서예인] 상품 상세 페이지
    상세(읽기 전용) 페이지

---
+++ SeoYein/frontend/src/components/lecture/product/JpaProductRead.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>상품 번호</td>
+        <td>
+          <input type="text" :value="product.productId" readonly/>
+        </td>
+      </tr>
+      <tr>
+        <td>상품명</td>
+        <td>
+          <input type="text" :value="product.productName" readonly/>
+        </td>
+      </tr>
+      <tr>
+        <td>작성자</td>
+        <td>
+          <input type="text" :value="product.writer" readonly/>
+        </td>
+      </tr>
+      <tr>
+        <td>등록일자</td>
+        <td>
+          <input type="text" :value="product.regDate" readonly/>
+        </td>
+      </tr>
+      <tr>
+        <td>본문</td>
+        <td>
+          <textarea cols="50" rows="20" :value="product.content" readonly/>
+        </td>
+      </tr>
+      <tr>
+        <td>가격</td>
+        <td>
+          <input type="number" :value="product.price" readonly/>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "JpaProductRead",
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ SeoYein/frontend/src/views/lecture/product/JpaProductReadPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <div align="center">
+      <h2>Vue + Spring 상품 게시판 읽기</h2>
+      <jpa-product-read v-if="product" :product="product"/>
+      <p v-else>로딩중 .......... </p>
+      <router-link :to="{ name: 'JpaProductModifyPage', params: { productId } }">
+        게시물 수정
+      </router-link>
+      <button @click="onDelete">삭제</button>
+      <router-link :to="{ name: 'JpaProductListPage' }">
+        돌아가기
+      </router-link>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+import JpaProductRead from '@/components/lecture/product/JpaProductRead.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  components: { JpaProductRead },
+    name: "JpaProductReadPage",
+    props: {
+        productId: {
+            type: String,
+            required: true,
+        }
+    },
+    computed: {
+        ...mapState(['product'])
+    },
+    methods: {
+        ...mapActions([
+            'requestProductToSpring',
+            'requestDeleteProductToSpring'
+        ]),
+        async onDelete () {
+            await this.requestDeleteProductToSpring(this.productId)
+            await this.$router.push({ name: 'JpaProductListPage' })
+        }
+    },
+    created () {
+        console.log('productId: ' + this.productId)
+        this.requestProductToSpring(this.productId)
+    }
+}
+
+</script>
+
+<style>
+
+</style>
