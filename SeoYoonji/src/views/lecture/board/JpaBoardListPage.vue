7644d6638a30ff2030c0b49150bc3e514b0470c9
dduru-nuna <syj00062@gmail.com>
2023-03-07 21:06:48 +0900

    fix: [서윤지] 게시물작성, 등록취소 라우터 바인드 누락 수정

--- SeoYoonji/src/views/lecture/board/JpaBoardListPage.vue
+++ SeoYoonji/src/views/lecture/board/JpaBoardListPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h2>Vue + Spring + JPA Boards</h2>
-    <router-link to="{ name: 'JpaBoardRegisterPage' }">
+    <router-link :to="{ name: 'JpaBoardRegisterPage' }">
         게시물 작성
     </router-link>
     <jpa-board-list :boards="boards"/>
