96d371a01c7ab9a877a20cea7b9a511628bf5a44
AnnSungHee <d980104@gmail.com>
2023-03-07 21:46:10 +0900

    add: [안성희] "JpaBoardModifyPage 주석 처리중"

--- AnnSungHee/src/store/actions.js
+++ AnnSungHee/src/store/actions.js
@@ -51,15 +51,6 @@ export default {
                 alert("문제 발생!")
             })
     },
-    requestProductListToSpring ({ commit }) {
-        return axios.get('http://localhost:7777/product/list')
-            .then((res) => {
-                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data)
-            })
-            // .catch(() => {
-            //     alert("이것도 못하니 멍청아?")
-            // })
-    },
     requestCreateProductToSpring ({}, payload) {
         const { name, price  } = payload
         return axios.post('http://localhost:7777/product/register',
@@ -69,6 +60,42 @@ export default {
             })
             .catch(() => {
                 alert('문제 발생!')
+            })
+    },
+    requestProductListToSpring ({ commit }) {
+        return axios.get('http://localhost:7777/product/list')
+            .then((res) => {
+                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data)
+            })
+            // .catch(() => {
+            //     alert("이것도 못하니 멍청아?")
+            // })
+    },
+    requestBoardToSpring ({ commit }, boardId) {
+        return axios.get(`http://localhost:7777/board/${boardId}`)
+            .then((res) => {
+                commit(REQUEST_BOARD_TO_SPRING, res.data)
+            })
+    },
+    requestDeleteBoardToSpring ({}, boardId) {
+        return axios.delete(`http://localhost:7777/board/${boardId}`)
+            .then(() => {
+                alert("삭제 성공")
+            })
+            .catch(() => {
+                alert("문제 발생!")
+            })
+    },
+    requestBoardModifyToSpring ({}, payload) {
+        const { title, content, boardId, writer } = payload
+
+        return axios.put(`http://localhost:7777/board/${boardId}`,
+            { title, writer, content })
+            .then(() => {
+                alert("수정 성공")
+            })
+            .catch(() => {
+                alert("문제 발생!")
             })
     },
 }
---
+++ AnnSungHee/src/views/lecture/product/JpaProductModifyPage.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-container>
+      <div align="center">
+        <h2>상품 수정</h2>
+        <!-- board 변수가 존재할 때에만 jpa-board-modify-form 컴포넌트가 렌더링  -->
+        <jpa-product-modify-form v-if="board" :product="product" @submit="onSubmit"/>
+        <p v-else>로딩중 ........</p>
+      </div>
+    </v-container>
+</template>
+  
+  <script>
+  
+  import JpaProductModifyForm from '@/components/lecture/board/JpaProductModifyForm.vue'
+  import { mapActions, mapState } from 'vuex'
+  
+  export default {
+      components: { JpaProductModifyForm },
+      name: "JpaBoardModifyPage",
+      props: {
+          productId: {
+              type: String,
+              required: true,
+            /*
+            router에서 parameter를 전달해서 각각의 다른 데이터를 가진 페이지로 이동할때
+            required: true라는 구문이 필요하다.
+            해당 view를 접근할때 해당하는 상품정보를 확인하기 위해 productId를 사용하였으니
+            부모로부터(접속을 실행한 페이지)에서 전달된 파라메터의 데이터타입을 명시하였다. 
+             */  
+          }
+      },
+      computed: {
+          ...mapState(['board'])
+          /*
+          computed 속성은 계산된 데이터를 반환하는 함수이며, 
+          mapState 함수를 사용하여 Vuex 스토어의 상태를 가져와 계산할 수 있습니다. 
+          이렇게 computed 속성을 사용하면 화면에 필요한 데이터를 쉽게 계산하고, 
+          필요한 상태를 간단하게 가져올 수 있습니다.
+          */
+      },
+      methods: {
+          ...mapActions([
+              'requestProductToSpring',
+              'requestProductModifyToSpring',
+              /*
+              나중에쓸 Vuex의 액션 함수들을 미리 선언해놔서 밑에서 쓸수있게 해두었다.
+              원래 해당 루트는 @/src/store/action.js안에 선언되어있다.
+              */
+             /*
+              이를 사용하면 Vuex 액션 함수를 Vuex 스토어에서 직접 호출하는 대신, 
+              해당 컴포넌트에서 액션 함수를 호출할 수 있습니다. 
+              이는 Vuex 스토어에 대한 의존성을 줄이고, 
+              코드를 더 간결하게 유지할 수 있도록 도와줍니다.
+              */
+          ]),
+          async onSubmit (payload) {    
+            // async : onSubmit 함수에대해 비동기처리를 실행한다.
+            // payload : JpaProductModifyForm 컴포넌트에서 실행된 수정된 데이터가 넘어와있다.
+              const { title, content, writer } = payload
+              const boardId = this.boardId
+  
+              await this.requestProductModifyToSpring({ ProductId, title, content, writer })
+              await this.$router.push({
+                  name: 'JpaProductReadPage',
+                  params: { boardId: this.boardId }
+              })
+          }
+      },
+      created () {
+          this.requestProductToSpring(this.boardId)
+      }
+  }
+  
+  </script>
+  
+  <style>
+  
+  </style>
