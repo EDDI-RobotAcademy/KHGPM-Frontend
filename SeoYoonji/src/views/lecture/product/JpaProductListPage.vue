9b5df548c168a98861d0296bb25e1e2631f83293
dduru-nuna <syj00062@gmail.com>
2023-03-07 21:40:09 +0900

    feat: [서윤지] 상품 목록 페이지 구현

---
+++ SeoYoonji/src/views/lecture/product/JpaProductListPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <h2>상품 페이지</h2>
+    <jpa-product-list :products="products"/>
+    <router-link :to="{ name: 'JpaProductRegisterPage' }">
+      상품 등록
+    </router-link>
+  </v-container>
+</template>
+
+<script>
+import JpaProductList from '@/components/lecture/product/JpaProductList.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+    components: { JpaProductList },
+    name: "JpaProductListPage",
+    computed: {
+        ...mapState([
+          'products'
+        ]),
+    },
+    mounted () {
+      this.requestProductListToSpring()
+    },
+    methods: {
+      ...mapActions([
+        'requestProductListToSpring'
+      ])
+    }
+}
+</script>
+
+<style>
+
+</style>
