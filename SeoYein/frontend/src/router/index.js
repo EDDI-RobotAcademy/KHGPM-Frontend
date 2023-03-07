e920d493583659da31b24e399c6c5bdbcea88d8b
Fromredtoviolet <aprilartists@gmail.com>
2023-03-07 23:41:31 +0900

    feat: [서예인] 상품 게시판 추가
    라우터 및 홈 뷰에 상품 게시판 추가 완료

--- SeoYein/frontend/src/router/index.js
+++ SeoYein/frontend/src/router/index.js
@@ -9,10 +9,16 @@ import ComponentTest from "@/views/lecture/componentTest/ComponentTest.vue"
 import EmitTestBoardGame from "@/views/lecture/emitTest/EmitTestBoardGame.vue"
 
 import AxiosTestPage from "@/views/lecture/axiosTest/AxiosTestPage.vue"
+
 import JpaBoardListPage from "@/views/lecture/board/JpaBoardListPage.vue"
 import JpaBoardRegisterPage from "@/views/lecture/board/JpaBoardRegisterPage.vue"
 import JpaBoardReadPage from "@/views/lecture/board/JpaBoardReadPage.vue"
 import JpaBoardModifyPage from "@/views/lecture/board/JpaBoardModifyPage.vue"
+
+import JpaProductListPage from "@/views/lecture/product/JpaProductListPage.vue"
+import JpaProductRegisterPage from "@/views/lecture/product/JpaProductRegisterPage.vue"
+import JpaProductReadPage from "@/views/lecture/product/JpaProductReadPage.vue"
+import JpaProductModifyPage from "@/views/lecture/product/JpaProductModifyPage.vue"
 
 Vue.use(VueRouter)
 
@@ -58,7 +64,7 @@ const routes = [
     component: JpaBoardRegisterPage
   },
   {
-    path: '/board-read/:boardId', // 콜론으로 가변인자 처리
+    path: '/board-read/:boardId',
     name: 'JpaBoardReadPage',
     components: {
       default: JpaBoardReadPage
@@ -68,7 +74,7 @@ const routes = [
     }
   },
   {
-    path: '/board-modify/:boardId', // 콜론으로 가변인자 처리
+    path: '/board-modify/:boardId',
     name: 'JpaBoardModifyPage',
     components: {
       default: JpaBoardModifyPage
@@ -77,6 +83,37 @@ const routes = [
       default: true
     }
   },
+  {
+    path: '/product-list-page',
+    name: 'JpaProductListPage',
+    component: JpaProductListPage
+  },
+  {
+    path: '/product-register',
+    name: 'JpaProductRegisterPage',
+    component: JpaProductRegisterPage
+  },
+  {
+    path: '/product-read/:productId',
+    name: 'JpaProductReadPage',
+    components: {
+      default: JpaProductReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/product-modify/:productId',
+    name: 'JpaProductModifyPage',
+    components: {
+      default: JpaProductModifyPage
+    },
+    props: {
+      default: true
+    }
+  }
+
   
 ]
 
