50224248112b856b1da119f94c74cb144e709071
jmj30040 <jmj30040@gmail.com>
2023-03-08 16:03:10 +0900

    feat: [정명진]Navigation Bar 만들기 [NAVIGATION]

--- JeongMyeongjin/src/App.vue
+++ JeongMyeongjin/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app>
+    <navigation-view/>
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
+
 <script>
+
+import NavigationView from './views/lecture/navigation/NavigationView.vue';
+
 export default {
   name: 'App',
   data: () => ({
     //
   }),
+  components: {
+    NavigationView
+  }
 };
 </script>
