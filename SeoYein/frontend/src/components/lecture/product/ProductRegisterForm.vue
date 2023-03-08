63f447671b65f88de9821af73091ba860b4f3c84
Fromredtoviolet <aprilartists@gmail.com>
2023-03-08 17:16:01 +0900

    feat: [서예인] 상품게시판 오탈자 수정
    오탈자 수정 반영

---
+++ SeoYein/frontend/src/components/lecture/product/ProductRegisterForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <!-- @submit.prevent 는 html에서 제공하는 기본 submit 기능을 막고, 
+    개발자가 직접 입력한 커스텀 메서드인 onSubmit을 사용하기 위해 쓴다. ($emit때문에?) -->
+    <table>
+        <tr>
+            <td>상품명</td>
+            <td>
+                <input type="text" v-model="productName"/>
+            </td>
+        </tr>
+        <tr>
+            <td>작성자</td>
+            <td>
+                <input type="text" v-model="writer"/>
+            </td>
+        </tr>
+        <tr>
+            <td>본문</td>
+            <td>
+                <textarea cols="50" rows="20" v-model="content"/>
+            </td>
+        </tr>
+        <tr>
+            <td>가격</td>
+            <td>
+                <input type="number" v-model="price"/>
+            </td>
+        </tr>
+    </table>
+    <div>
+        <button type="submit">등록</button>
+        <router-link :to="{ name: 'ProductListPage' }">
+            취소
+        </router-link>
+    </div>
+  </form>
+</template>
+
+<script>
+
+export default {
+    name: "ProductRegisterForm",
+    data () {
+        return {
+            productName: '상품명을 입력하세요.',
+            writer: '누구세요?',
+            content: '내용을 입력하세요.',
+            price: 0,
+        }
+    },
+    methods: {
+        onSubmit () {
+            const { productName, writer, content, price } = this
+            this.$emit('submit', { productName, writer, content, price })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ SeoYein/frontend/src/views/lecture/product/ProductRegisterPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <h2>게시물 작성</h2>
+    <product-register-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+
+import ProductRegisterForm from '@/components/lecture/product/ProductRegisterForm.vue'
+import { mapActions } from 'vuex'
+        // 맵액션즈는 엑시오스 스토어에 있는 액션이랑 매핑됨
+
+export default {
+    components: { ProductRegisterForm },
+    name: "ProductRegisterPage",
+    methods: {
+        ...mapActions ([
+            'requestCreateProductToSpring'
+        ]),
+        async onSubmit (payload) {
+            await this.requestCreateProductToSpring(payload)
+            // this.~~가 끝날때까지 기다리라는 뜻의 await
+            await this.$router.push({
+              // $는 view가 라우터 정보를 제어하기 위함. 강제로 다른 페이지로 이동
+              name: 'ProductListPage'
+            })
+        }
+    }
+}
+</script>
+
+<style>
+</style>
