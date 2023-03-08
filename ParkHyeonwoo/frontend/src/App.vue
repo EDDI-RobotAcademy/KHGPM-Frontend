f6c42566bb0fb76dd04d0e11cb54cf93c81d79c1
Hyeonvv <pphhww5017@gmail.com>
2023-03-08 17:00:30 +0900

    feat: Footer 추가

--- ParkHyeonwoo/frontend/src/App.vue
+++ ParkHyeonwoo/frontend/src/App.vue
@@ -2,18 +2,20 @@
   <v-app>
     <navigation-menu-page/> <!-- 프로젝트 내 모든 Vue 파일에 적용된다. -->
     <v-main>
-      <router-view/> <!-- HomeView.vue 와 비슷한 기능을 한다고 생각 -->
+      <router-view/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
-import NavigationMenuPage from './views/lecture/navigation/NavigationMenuPage.vue';
+import Footer from '@/views/lecture/layoutForm/Footer.vue';
+import NavigationMenuPage from '@/views/lecture/layoutForm/navigation/NavigationMenuPage.vue';
 
 // import NavigationView from './views/lecture/navigation/NavigationView.vue';
 
 export default {
-  components: { NavigationMenuPage },
+  components: { NavigationMenuPage, Footer },
   name: 'App',
   data: () => ({
     //
