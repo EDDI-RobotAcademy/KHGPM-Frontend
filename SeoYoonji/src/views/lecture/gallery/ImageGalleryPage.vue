fa11fe070c3ebed53a56f86fce599c77bd5d9f17
dduru-nuna <syj00062@gmail.com>
2023-03-08 21:21:02 +0900

    docs: [서윤지] 업로드되는 이미지 가변 처리 주석

--- SeoYoonji/src/views/lecture/gallery/ImageGalleryPage.vue
+++ SeoYoonji/src/views/lecture/gallery/ImageGalleryPage.vue
@@ -3,6 +3,8 @@
     <v-row>
         <v-col v-for="image in images" :key="image" cols="3">
             <v-img :src="image" aspect-ratio="1" class="grey lighten-2">
+            <!--가변인자 처리 ``(백틱) 사용. :src="require(`@/assets/imgs/${imageNmae}`)"-->
+            <!--state 에 images, image 설정하여 backend에서 넘어오는 이미지 받으면 된다-->    
                 <!--로딩중일 때 보여지는 부분-->
                 <template v-slot:placeholder>
                     <v-row class="fill-height ma-0" align="center" justify="center">
@@ -22,7 +24,12 @@ export default {
         return {
             images: [
                 require('@/assets/imgs/KakaoTalk_20230308_172908690.jpg'),
-                require('@/assets/imgs/KakaoTalk_20230308_175848736.jpg')
+                require('@/assets/imgs/KakaoTalk_20230308_175848736.jpg'),
+                require('@/assets/imgs/angel.gif'),
+                require('@/assets/imgs/abheart.gif'),
+                require('@/assets/imgs/k1.gif'),
+                require('@/assets/imgs/bird.gif'),
+                require('@/assets/imgs/cat.gif'),
             ]
         }
     },
