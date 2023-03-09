914c280f9f021a5d06f3037d7e45e04874248935
jmj30040 <jmj30040@gmail.com>
2023-03-10 04:11:14 +0900

    add: [정명진] 변수를 연동한 이미지 경로 설정 [GALLERYx]

--- JeongMyeongjin/frontend/src/views/lecture/gallery/ImageGalleryPage.vue
+++ JeongMyeongjin/frontend/src/views/lecture/gallery/ImageGalleryPage.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
       <v-row>
-        <v-col v-for="image in images" :key="image" cols="3">
+        <v-col v-for="(image, idx) in images" :key="idx" cols="3">
           <v-img :src="image" aspect-ratio="1" class="grey lighten-2">
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
@@ -11,6 +11,9 @@
           </v-img>
         </v-col>
       </v-row>
+      <!-- 보편적으로 플젝을 하는 상황에서 제일 많이 경험하는 문제 파트임 -->
+     <v-img :src="require(`@/assets/uploadImgs/${imageName}`)" 
+     aspect-ratio="1" class="grey lighten-2"/>
     </v-container>
   </template>
  
@@ -19,15 +22,16 @@
       name: "ImageGalleryPage",
       data () {
           return {
-              images: [
-                  require('@/assets/uploadImgs/20171225_234934.jpg'),
-                  require('@/assets/uploadImgs/IMG_4374.jpg'),
-                  require('@/assets/uploadImgs/img.jpg'),
-                  ]
+            imageName: "link.jpg",
+            images: [
+                require('@/assets/uploadImgs/20171225_234934.jpg'),
+                require('@/assets/uploadImgs/IMG_4374.jpg'),
+                require('@/assets/uploadImgs/img.jpg'),
+                ]
          }
      }
  }
  </script>
 
- <style>
+ <style scoped>
  </style>
