479599de0800f1f3cd7e970557673ddff61cb6bd
jonginhan <instantgogo@gmail.com>
2023-03-11 17:49:26 +0900

    fix: [한종인] 라우터 수정

--- JongInHan/Frontend/src/views/HomeView.vue
+++ JongInHan/Frontend/src/views/HomeView.vue
@@ -9,7 +9,7 @@
         <router-link to="/emit-test-board-game">emit 테스트(보드 게임)</router-link> |
         <router-link to="/axios-test-page">Vue에서 Spring으로 데이터 전송하기</router-link> |
         <router-link to="/board-list-page">게시판</router-link> |
-        <router-link to="/product-list-page">상품 게시판</router-link> |
+        <router-link to="/shop-list-page">상품 게시판</router-link> |
         <router-link to="/file-upload">파일 업로드</router-link> | <br/>
         <router-link to="/image-gallery">이미지 갤러리</router-link> |
         <router-link to="/life-cycle">라이프 사이클</router-link> |
