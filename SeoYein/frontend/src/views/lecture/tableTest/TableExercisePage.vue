ea4e724ef0f683c2cd80d4e60bf080c3db52be67
Fromredtoviolet <aprilartists@gmail.com>
2023-03-12 23:22:51 +0900

    add: [서예인] 0309 학습 - 주석 추가
    용어 설명 주석 추가

--- SeoYein/frontend/src/views/lecture/tableTest/TableExercisePage.vue
+++ SeoYein/frontend/src/views/lecture/tableTest/TableExercisePage.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container>
+    <!-- 
+      headers : 각각 헤더 열을 설명하는 개체의 배열
+      items : 표시할 항목의 배열
+      show-select : 체크박스 역할
+      boardId(item-key) : spring에서 넘어온 boards정보의 boardId
+      selectedItems(v-model) : 체크한 항목들과 selectedItems 배열 바인딩
+    -->
     <v-data-table 
         :headers="headerTitle" 
         :items="boards"
@@ -19,7 +26,7 @@ export default {
     name: "TableExercisePage",
     data () {
         return {
-            headerTitle: [
+            headerTitle: [ // value 값은 spring 쪽 entity 변수명과 같아야 한다.
                 { text: '번호', value: 'boardId', width: "70px" },
                 { text: '제목', value: 'title', width: "200px" },
                 { text: '작성자', value: 'writer', width: "100px" },
@@ -43,7 +50,8 @@ export default {
         ]),
         readRow (readValue) {
             alert("게시물 읽기: " + JSON.stringify(readValue))
-            router.push({
+            // JSON.stringify( )는 자바스크립트의 값을 JSON 문자열로 변환
+            router.push({ // JpaBoardReadPage 로 이동, boardId 정보 같이 넘김
                 name: 'JpaBoardReadPage',
                 params: { boardId: readValue.boardId.toString() }
             })
