b7b5c917202b4b86f92581eb65d960a27a895471
jeongwoogithub <leejeongpop@gmail.com>
2023-03-13 18:47:55 +0900

    feat:[이정우]이미지 클릭 시 상세보기 창 기능 구현

--- Leejeongwoo/src/views/lecture/gallery/ImageGalleryPage.vue
+++ Leejeongwoo/src/views/lecture/gallery/ImageGalleryPage.vue
@@ -1,29 +1,29 @@
 <template>
   <v-container>
-
-
-    <!-- <div class="black-bg" v-if="imageread = true">
-      <div v-for="image in images" :key="image" class="white-bg"></div>
-      <button @click="imageread = false">닫기</button>
-    </div> -->
-
-
+    <v-card class="black-bg" v-if="showPreview">
+      <v-card-text class="preview-container" @click="showPreview = false">
+        <v-img :src="previewImage" aspect-ratio="1" class="white-bg">
+          <template v-slot:placeholder>
+            <v-progress-circular indeterminate color="lighten-5-grey"/>
+          </template>
+        </v-img>
+      </v-card-text>
+    </v-card>
     <v-row>
       <v-col v-for="(image, idx) in images" :key="idx" cols="3">
-        <v-img :src="image" aspect-ratio="1" class="grey lighten-2">
+        <v-img :src="image" aspect-ratio="1" class="light gray-2" @click="previewImage = image; showPreview = true">
           <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey lighten-5"/>
-            </v-row>
+            <v-progress-circular indeterminate color="lighten-5-grey"/>
           </template>
         </v-img>
       </v-col>
     </v-row>
-    <!-- 보편적으로 플젝을 하는 상황에서 제일 많이 경험하는 문제 파트임 -->
-    <!-- <v-img :src="require(`@/assets/uploadImgs/${imageName}`)" 
-            aspect-ratio="1" class="grey lighten-2"/> -->
+
   </v-container>
 </template>
+<!-- 보편적으로 플젝을 하는 상황에서 제일 많이 경험하는 문제 파트임 -->
+<!-- <v-img :src="require(`@/assets/uploadImgs/${imageName}`)" 
+        aspect-ratio="1" class="grey lighten-2"/> -->
 
 <script>
 export default {
@@ -31,6 +31,7 @@ export default {
   data() {
     return {
       // imageread: false,
+      // imageName: "link.jpg",
       images: [
         require('@/assets/uploadImgs/하민1.jpg'),
         require('@/assets/uploadImgs/하민2.jpg'),
@@ -38,38 +39,39 @@ export default {
         require('@/assets/uploadImgs/하민4.jpg'),
         require('@/assets/uploadImgs/하민5.jpg'),
         require('@/assets/uploadImgs/하민6.jpg'),
+        require('@/assets/uploadImgs/하민7.jpg'),
         require('@/assets/uploadImgs/하민1.jpg'),
         require('@/assets/uploadImgs/하민2.jpg'),
         require('@/assets/uploadImgs/하민3.jpg'),
         require('@/assets/uploadImgs/하민4.jpg'),
         require('@/assets/uploadImgs/하민5.jpg'),
         require('@/assets/uploadImgs/하민6.jpg'),
+        require('@/assets/uploadImgs/하민7.jpg'),
         require('@/assets/uploadImgs/하민1.jpg'),
         require('@/assets/uploadImgs/하민2.jpg'),
         require('@/assets/uploadImgs/하민3.jpg'),
         require('@/assets/uploadImgs/하민4.jpg'),
         require('@/assets/uploadImgs/하민5.jpg'),
         require('@/assets/uploadImgs/하민6.jpg'),
-      ]
+        require('@/assets/uploadImgs/하민7.jpg'),
+      ],
+      showPreview: false,
+      previewImage: ""
     }
   }
 }
 </script>
 
 <style scoped>
-
-.black-bg {
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+.preview-container {
   position: fixed;
-  padding: 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  z-index: 9999;
+  max-width: 800px; /* adjust the value as needed */
+  margin: auto;
 }
-
-.white-bg {
-  width: 100%;
-  background: white;
-  border-radius: 8px;
-  padding: 20px;
-}
-</style>
+</style>x
