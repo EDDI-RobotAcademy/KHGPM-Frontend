687c829c3443253bb08206b5b2458d317f18f35f
jeongwoogithub <leejeongpop@gmail.com>
2023-03-13 21:38:28 +0900

    feat:[이정우]게시판 등록 시 상세페이지 이동 기능 추가

--- Leejeongwoo/src/views/lecture/board/JpaBoardListPage.vue
+++ Leejeongwoo/src/views/lecture/board/JpaBoardListPage.vue
@@ -1,39 +1,64 @@
 <template>
-  <v-container>
-    <h2>Vue + Spring + JPA Boards</h2>
-    <p><mark>게시글 목록이 보여지는 페이지 입니다.</mark></p>
-    <router-link :to="{ name: 'JpaBoardRegisterPage' }">
-      게시글 작성 하러 가기
-    </router-link>
-    <jpa-board-list :boards="boards"/>
-  </v-container>
+    <v-container>
+        <div align="center">
+            <h2>Vue + Spring + JPA Boards</h2>
+            <p><mark>게시글 목록이 보여지는 페이지 입니다.</mark></p>
+            <router-link :to="{ name: 'JpaBoardRegisterPage' }">
+                게시글 작성 하러 가기
+            </router-link>
+        </div>
+        <v-data-table :headers="headerTitle" :items="boards" :items-per-page="10" @click:row="readRow" show-select
+            class="elevation-1" item-key="boardId" v-model="selectedItems" />
+        <!-- <jpa-board-list :boards="boards"/> -->
+    </v-container>
 </template>
 
 <script>
 
 import JpaBoardList from '@/components/lecture/board/JpaBoardList.vue'
+import router from "@/router"
+
 //vuex는 상태(state)를 관리하는 라이브러리
 import { mapActions, mapState } from 'vuex'
 
 export default {
-  components: { JpaBoardList },
-  name: "JpaBoardListPage",
-  computed: {
-    ...mapState([
-      'boards'
-    ]),
-  },
-  mounted () {
-    this.requestBoardListToSpring()
-  },
-  methods: {
-    ...mapActions([
-      'requestBoardListToSpring'
-    ])
-  }
+    components: { JpaBoardList },
+    name: "JpaBoardListPage",
+    data() {
+        return {
+            headerTitle: [
+                { text: '번호', value: 'boardId', width: "70px" },
+                { text: '제목', value: 'title', width: "200px" },
+                { text: '작성자', value: 'writer', width: "100px" },
+                { text: '등록일자', value: 'regDate', width: "100px" },
+            ],
+            selectedItems: [],
+            //boardsWithSelected: [this.boards.map(x => ({...x, isSelectable: false}))],
+        }
+    },
+    computed: {
+        ...mapState([
+            'boards'
+        ]),
+    },
+    mounted() {
+        this.requestBoardListToSpring()
+    },
+    methods: {
+        ...mapActions([
+            'requestBoardListToSpring'
+        ]),
+        // 사용자가 테이블의 특정 데이터 행을 클릭할 때 호출됨
+        readRow(readValue) {
+            router.push({
+                name: 'JpaBoardReadPage',
+                params: { boardId: readValue.boardId.toString() }
+            })
+        },
+    }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
