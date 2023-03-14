51b3f66f8e9b304c13838e0996158f71a8b28f20
dduru-nuna <syj00062@gmail.com>
2023-03-14 21:44:01 +0900

    feat: [서윤지] 상품게시판 사진 등록 구현중

--- SeoYoonji/src/components/lecture/product/JpaProductRegisterForm.vue
+++ SeoYoonji/src/components/lecture/product/JpaProductRegisterForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="onSubmit">
+    <form @submit.prevent="onSubmit" enctype="multipart/form-data">
       <table>
           <tr>
               <td>제품명</td>
@@ -19,10 +19,16 @@
                   <textarea cols="30" rows="10" v-model="detail"/>
               </td>
           </tr>
+          <tr>
+            <td>상품 사진</td>
+            <td>
+                  <input type="file" id="productImgs" ref="productImgs" multiple @change="handleFileUpload"/>
+            </td>
+        </tr>
       </table>
   
       <div>
-          <button type="submit">등록</button>
+          <button @click="onSubmit">등록</button>
           <router-link :to="{ name: 'JpaProductListPage' }">
               취소
           </router-link>
@@ -31,6 +37,8 @@
   </template>
   
   <script>
+  import axios from 'axios'
+
   export default {
       name: "JpaProductRegisterForm",
       data () {
@@ -41,10 +49,25 @@
           }
       },
       methods: {
+          handleFileUpload () {
+            this.productImgs = this.$refs.productImgs.files
+          },
           onSubmit () {
-              const { title, price, detail } = this
-              this.$emit('submit', { title, price, detail })
-          }
+              let formData = new FormData()
+              let fileinfo = { title: this.title, price: this.price, detail: this.detail }
+              for(let idx = 0; idx < this.productImgs.length; idx++) {
+                  formData.append('productImgList', this.productImgs[idx])
+                }
+              formData.append("info", new Blob([JSON.stringify(fileinfo)], { type: "application/json" }))
+              axios.post('http://localhost:7777/product/register', formData)
+                .then (res => {
+                    alert('처리 결과: ' + res.data)
+                    this.$emit('submit', formData)
+                })
+                .catch(() => {
+                    alert('문제 발생!')
+                })
+            }
       }
   }
   </script>
