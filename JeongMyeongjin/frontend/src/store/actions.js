fc2bf2334bc22294730de27e79bf7d218ce525fb
jmj30040 <jmj30040@gmail.com>
2023-03-15 21:54:26 +0900

    feat: [정명진] 상품 이미지 조회 기능 [PRODUCT-IMAGEx]

--- JeongMyeongjin/frontend/src/store/actions.js
+++ JeongMyeongjin/frontend/src/store/actions.js
@@ -3,6 +3,7 @@ import {
     REQUEST_BOARD_TO_SPRING,
     REQUEST_PRODUCT_LIST_TO_SPRING,
     REQUEST_PRODUCT_TO_SPRING,
+    REQUEST_PRODUCT_IMAGE_LIST_TO_SPRING,
 } from './mutation-types'
 
 import axios from 'axios'
@@ -99,4 +100,10 @@ export default {
                 alert("문제 발생!")
             })
     },
+    requestProductImageToSpring ({ commit }, productId) {
+        return axios.get(`http://localhost:7777/product/imageList/${productId}`)
+            .then((res) => {
+                commit(REQUEST_PRODUCT_IMAGE_LIST_TO_SPRING, res.data)
+            })
+    }
 }
